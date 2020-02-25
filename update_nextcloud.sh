0af6bd33eaf6c8f2dd43e7000fe2552eb816d277
Roberto Marzocchi <roberto.marzocchi@gter.it>
2020-02-25 17:27:44 +0100

    add file

---
+++ update_nextcloud.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# GTER Copyleft
+# this script is used to update content of nextcloud folder which is added by a python script on the server
+
+cd /var/www/html/nextcloud/
+sudo -u www-data php console.php files:scan --path="concerteaux/files/vector"
