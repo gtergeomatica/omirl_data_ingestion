bbad20fdb146df7938835a640445181a46b5b2e2
Roberto Marzocchi <roberto.marzocchi@gter.it>
2020-02-25 16:47:46 +0100

    Salva il file nella cartella dello script

--- json2geojson.py
+++ json2geojson.py
@@ -8,9 +8,11 @@
 # Gter copyleft 2020
 # author: Roberto Marzocchi
 ###############################################################################
+import os,sys
 
 from sys import argv
-from os.path import exists
+#from os.path import exists
+
 import simplejson as json 
 
 import urllib.request, json 
@@ -44,10 +46,11 @@ geojson = {
         "properties" : d,
      } for d in data]
 }
+#with open(os.path.join(sys.path[0], "my_file.txt"), "r") as f:
 
 out_file="{}.json".format(input1)
 print(out_file)
-output = open(out_file, 'w')
+output = open(os.path.join(sys.path[0],out_file), 'w')
 json.dump(geojson, output)
 #print(geojson)
 
