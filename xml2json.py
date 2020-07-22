b9cf3362bad63c797dfbef8419f2427ddf7633da
Roberto Marzocchi <gter@gter.it>
2020-07-22 17:44:54 +0200

    Modificato file xml2json per gestire correttamente i null

--- xml2json.py
+++ xml2json.py
@@ -1,10 +1,8 @@
 #! usr/bin/env python
 #Script per convertire gli XML per i grafici della rete OMIRL in json per highchart
 # si lancia in maniera diversa a seconda dei dati che si intendono recuperare
-# - python3 json2geojson.py Pluvio
-# - python3 json2geojson.py Termo
-# - python3 json2geojson.py Idro
-# - python3 json2geojson.py Press
+# - python3 xml2json.py Idro MONTG
+# il file legge l'XML usato per i grafici OMIRL e crea un json per Highcharts
 # Gter copyleft 2020
 # author: Roberto Marzocchi
 ###############################################################################
@@ -53,64 +51,24 @@ output.write('[')
 comma_count0=0
 for dataSeries in root[5]:
     if dataSeries.tag =='data':
-        if comma_count0 > 0:
-            output.write(',')
-        comma_count0 += 1
-        output.write('[')
-        comma_count1=0
-        for item in dataSeries:
-            if comma_count1>0:
+        if dataSeries[1].text != None:
+            if comma_count0 > 0:
                 output.write(',')
-            output.write(item.text)
-            comma_count1+=1
-        output.write(']')
+            comma_count0 += 1
+            
+            output.write('[')
+            comma_count1=0
+            for item in dataSeries:
+                if comma_count1>0:
+                    output.write(',')
+                try:
+                    output.write(item.text)
+                except:
+                    output.write('0')
+                comma_count1+=1
+            output.write(']')
+        else:
+            print('Dato mancante')
 output.write(']')
 
 exit()
-
-print("DATI LETTI CORRETTAMENTE")
-#exit()
-#script, in_file, out_file = argv
-
-
-print(len(data))
-
-#refDate = 2020-02-27T09:15:00
-
-
-data_reference=datetime.datetime.utcnow() - datetime.timedelta(minutes=60)
-
-print(data_reference)
-
-#date_time_obj = datetime.datetime.strptime(date_time_str, '%b %d %Y %I:%M%p')
-
-# Filter python objects with list comprehensions
-data_updated = [x for x in data if datetime.datetime.strptime(x['refDate'],'%Y-%m-%dT%H:%M:%S') > data_reference ]
-
-
-print(len(data_updated))
-
-#exit()
-
-geojson = {
-    "type": "FeatureCollection",
-    "features": [
-    {
-        "type": "Feature",
-        "geometry" : {
-            "type": "Point",
-            "coordinates": [d["lon"], d["lat"]],
-            },
-        "properties" : d,
-     } for d in data_updated]
-}
-#with open(os.path.join(sys.path[0], "my_file.txt"), "r") as f:
-
-out_file="{}.json".format(input1)
-print(out_file)
-output = open(os.path.join(sys.path[0],out_file), 'w')
-json.dump(geojson, output)
-#print(geojson)
-
-
-
